837e1d7f5b5c2e4d04754303015c10e9148e50cd
oliv <mister.devo@gmail.com>
2016-03-04 22:58:22 +0100

    first commit 1.0.0

---
+++ test/test-spec.js
@@ -0,0 +1,59 @@
+var assert = require('assert');
+var test = require('selenium-webdriver/testing'),
+    webdriver = require('selenium-webdriver');
+
+var options = require('./trav-sl-opt.js');
+options.baseUrl:'https://www.google.com'
+
+// var options = {
+//   server:'http://127.0.0.1:4444/wd/hub',
+//   desiredCapabilities:{browserName:'firefox'},
+//   baseUrl:'https://www.google.com'
+// };
+
+test.describe('End To End tests example', function() {
+
+    this.timeout(60000);
+    var client = {};
+
+    test.before(function(){
+        client = new webdriver.Builder().
+                          withCapabilities(options.desiredCapabilities).
+                          usingServer(options.server).
+                          build();
+    });
+
+
+    test.describe('Fake Google Test example', function() {
+
+        test.it('should have the right title', function () {
+            client.get(options.baseUrl);
+            client.wait(client.getTitle(), 10000)
+              .then(function(title){
+                  assert.equal(title, 'Google');
+              });
+        });
+
+    });
+
+    var passed = true;
+    test.afterEach(function() {
+        if(this.currentTest.state === 'failed') {
+          passed = false;
+        }
+    });
+
+    test.after(function(done) {
+        if(options.saucelabs){
+          client.getSession().then(function (sessionid){
+            options.saucelabs.updateJob(sessionid.id_,
+                                          { passed: passed },
+                                          function(){ client.quit(); done(); });
+          });
+        } else {
+            client.quit();
+            done();
+        }
+    });
+
+});
---
+++ test/trav-sl-opt.js
@@ -0,0 +1,32 @@
+var SauceLabs = require("saucelabs");
+var username = process.env.SAUCE_USERNAME,
+    accessKey = process.env.SAUCE_ACCESS_KEY;
+
+var saucelabs = new SauceLabs({
+            username: username,
+            password: accessKey
+          });
+
+module.exports = {
+        server : "http://" + username + ":" + accessKey +
+            "@ondemand.saucelabs.com:80/wd/hub",
+
+        saucelabs: saucelabs,
+
+        logLevel: 'silent',
+
+        desiredCapabilities: {
+            browserName: (process.env._BROWSER || '').replace(/_/g, ' '),
+            platform: (process.env._PLATFORM || '').replace(/_/g, ' '),
+            version: process.env._VERSION,
+
+            tags: [process.env.TRAVIS_JOB_NUMBER],
+            name:  process.env.TRAVIS_JOB_NUMBER,
+            build: process.env.TRAVIS_BUILD_NUMBER,
+
+            passed: 'false',
+
+            'public': true
+        }
+
+    }
