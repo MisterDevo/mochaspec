2be11281868cf22f49413c8186b0f14551f9541c
oliv <mister.devo@gmail.com>
2016-03-05 00:36:46 +0100

    rollback

--- test/test-spec.js
+++ test/test-spec.js
@@ -46,14 +46,11 @@ test.describe('End To End tests example', function() {
     test.after(function(done) {
         if(options.saucelabs){
             client.getSession().then(function (sessionid){
-              options.saucelabs.updateJob( sessionid.id_, { passed: passed }, function(err, res) {
-                if(err){
-                  //retry
-                  options.saucelabs.updateJob( sessionid.id_, { passed: passed }, function(err, res) {});
-                }
+              options.saucelabs.updateJob( sessionid.id_, { passed: passed }, function(done) {
+                client.quit();
+                done();
               });
-              client.quit();
-              done();
+
             });
         } else {
             client.quit();
