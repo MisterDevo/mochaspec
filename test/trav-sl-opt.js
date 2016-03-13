b79aa018dc43c6c42c2e05e0eb3f463935108950
oliv <mister.devo@gmail.com>
2016-03-13 22:33:14 +0100

    change version browser by latest

--- test/trav-sl-opt.js
+++ test/trav-sl-opt.js
@@ -18,7 +18,7 @@ module.exports = {
         desiredCapabilities: {
             browserName: (process.env._BROWSER || '').replace(/_/g, ' '),
             platform: (process.env._PLATFORM || '').replace(/_/g, ' '),
-            version: process.env._VERSION,
+            version: 'latest',
 
             tags: [process.env.TRAVIS_JOB_NUMBER],
             name:  process.env.TRAVIS_JOB_NUMBER,
